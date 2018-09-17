980d088444a3d199adc9bb6cdc94f9fc4c836a7a
sugarshin <shinsugar@gmail.com>
2018-09-17 22:23:18 +0900

    Base impl

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,17 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-koa'
+import * as Koa from 'koa'
+import { typeDefs, resolvers } from './schema'
 
-// This is a (sample) collection of books we'll be able to query
-// the GraphQL server for.  A more complete example might fetch
-// from an existing data source like a REST API or database.
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-]
-
-// Type definitions define the "shape" of your data and specify
-// which ways the data can be fetched from the GraphQL server.
-const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-
-  # This "Book" type can be used in other type declarations.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
-  type Query {
-    books: [Book]
-  }
-`
-
-// Resolvers define the technique for fetching the types in the
-// schema.  We'll retrieve books from the "books" array above.
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-}
-
-// In the most basic sense, the ApolloServer can be started
-// by passing type definitions (typeDefs) and the resolvers
-// responsible for fetching the data for those types.
 const server = new ApolloServer({ typeDefs, resolvers })
+const app = new Koa()
+const {
+  PORT: port = 4000,
+  GRAPHQL_PATH: graphqlPath = '/graphql',
+  APP_ORIGIN: origin = 'http://localhost',
+} = process.env
 
-// This `listen` method launches a web-server.  Existing apps
-// can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`)
+server.applyMiddleware({ app, path: graphqlPath })
+
+app.listen({ port }, () => {
+  console.log(`🚀 Server ready at ${origin}:${port}${server.graphqlPath}`)
 })
---
+++ src/schema/index.ts
@@ -0,0 +1,41 @@
+import { gql } from 'apollo-server-koa'
+
+export const typeDefs = gql`
+
+type CouponUrl {
+  desktop: String
+  mobile: String
+}
+
+type Restaurant {
+  id: String!
+  name: String!
+  name_kana: String!
+  latitude: String!
+  longitude: String!
+  url: String!
+  url_mobile: String
+  images: [String]!
+  coupon_url: CouponUrl!
+  tel: String
+  opening_times: String
+  catchphrase: String
+  description: String
+  access: String
+  holiday: String
+  credit_card: String
+  non_smoking: String
+  lunch: String
+  children: String
+}
+
+type Query {
+  restaurants: [Restaurant]
+}
+`
+
+export const resolvers = {
+  Query: {
+    restaurants: () => [], // tmp
+  },
+}

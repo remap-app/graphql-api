9557492ae7667b3a5572de168901ec7ba7d27bbe
sugarshin <shinsugar@gmail.com>
2018-09-22 11:34:28 +0900

    Authentication

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { ApolloServer } from 'apollo-server-micro'
 import * as compose from 'micro-compose'
 import { handleErrors } from 'micro-errors'
 import * as cors from 'micro-cors-multiple-allow-origin'
+import * as auhtenticateMiddleware from '@remap/authenticate-middleware'
+import authenticationService from './services/authentication'
 import { typeDefs, resolvers } from './schema'
 
 const { CORS_ALLOWED_ORIGINS = '', NODE_ENV = 'development' } = process.env
@@ -14,4 +16,5 @@ module.exports = compose(
     allowMethods: ['GET', 'POST', 'OPTIONS'],
     origin: CORS_ALLOWED_ORIGINS.split(','),
   }),
+  auhtenticateMiddleware({ authenticate: authenticationService }),
 )(apolloServer.createHandler())
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,3 +1,13 @@
 declare module 'micro-cors-multiple-allow-origin'
 declare module 'micro-compose'
 declare module 'micro-errors'
+declare module '@remap/authenticate-middleware'
+
+interface IAuthentication {
+  name: string;
+  picture: string;
+  auth_time: number;
+  email: string;
+  email_verified: boolean;
+  uid: string;
+}
